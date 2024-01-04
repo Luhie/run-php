fc49ec2f553eaee07a96806c1f2756e0eeb5d398
luhie <nerdrun.luhie@gmail.com>
2024-01-05 00:01:52 +0900

    make instance variable

--- oop/class_instance.php
+++ oop/class_instance.php
@@ -1,11 +1,28 @@
 <?php
 class MyFileObejct{
   function isFile(){
-    return is_file('data.txt');
+    return is_file($this->filename);
   }
 }
 
 
 $file =  new MyFileObejct();
+$file->filename = 'data.txt';
 var_dump($file->isFile());
+var_dump($file->filename);
+
+$file2 =  new MyFileObejct();
+$file2->filename = 'data2.txt';
+var_dump($file2->isFile());
+var_dump($file2->filename);
+
+
+
+
+/*
+MyFileObject : Class
+$file, $file2 : Instance
+isFile : Method, behavior
+$this -> filename : Instance variable/field, property / status
+*/
 ?>
