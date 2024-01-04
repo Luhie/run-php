e5b69201831d879bfc704b45347975996719bef5
Luhie <nerdrun.luhie@gmail.com>
2024-01-04 17:38:10 +0900

    compare function and obejct

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # run-php
+
+## Function & Object
+- is_file();
+- is_dir();
+- file_get_contents();
+- file_put_contents('', '');
+- new splFileObject('');
+- isFile();
+- isDir();
+- fread($file->getSize());
+- fwrite();
---
+++ oop/compare.php
@@ -0,0 +1,27 @@
+<h1>Fucntion</h1>
+<?php 
+// 일반적 파일이라면 true를 리턴한다. 
+var_dump(is_file('data.txt'));
+var_dump(is_dir('data.txt'));
+var_dump(file_get_contents('data.txt'));
+file_put_contents('data.txt', rand(1,100));
+// 파일을 제어할 수 있는 함수
+// 함수를 통한 파일 핸들링
+// procedural
+?>
+<h1>Object</h1>
+<?php
+//객체 리턴
+$file = new SplFileObject('data.txt');
+var_dump($file -> isFile());
+var_dump($file -> isDir());
+var_dump($file -> fread($file -> getSize()));
+$file -> fwrite(rand(1,100));
+
+/********************* 
+SplFileObject : class
+$file : instance
+isFIle, isDir, fread : Method
+data.txt : state
+*************************/
+?>
