c7b43711112bd876976b30ed0434cdca16437b75
luhie <nerdrun.luhie@gmail.com>
2024-01-05 00:37:05 +0900

    encapsulation use private

--- README.md
+++ README.md
@@ -20,3 +20,4 @@
 
 ## Constructor
 - function __construct(){}
+- private
--- oop/class_instance.php
+++ oop/class_instance.php
@@ -1,7 +1,11 @@
 <?php
 class MyFileObejct{
+  private $file_name;
   function __construct($fname){
     $this->file_name = $fname;
+    if(!file_exists($this->file_name)){
+      die('There is no file'.$this->file_name);
+    }
   }
   // public $file_name;
   function isFile(){
@@ -11,6 +15,7 @@ class MyFileObejct{
 
 
 $file =  new MyFileObejct('data.txt');
+// $file =  new MyFileObejct();
 // $file->file_name = 'data.txt';
 var_dump($file->isFile());
 var_dump($file->file_name);
