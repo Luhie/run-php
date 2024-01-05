970046b495d76b9500d4df6579d0a908fd5094c2
luhie <nerdrun.luhie@gmail.com>
2024-01-05 18:24:10 +0900

    static, compare $this self::

--- oop/static/person.php
+++ oop/static/person.php
@@ -1,13 +1,16 @@
 <?php
 class Person{
-  private $count = 0;
+  private static $count = 0;
   private $name;
   function __construct($name){
     $this->name = $name;
-    $this->count = $this->count + 1;
+    self::$count = self::$count + 1;
   }
   function enter(){
-    echo "<h1>Enter ".$this->name." {$this->count}th</h1>";
+    echo "<h1>Enter ".$this->name." ".self::$count."th</h1>";
+  }
+  static function getCount(){
+    return self::$count;
   }
 
 }
@@ -23,5 +26,7 @@ $p3->enter();
 
 $p4 = new Person('2b');
 $p4->enter();
+
+echo Person::getCount();
 
 ?>
